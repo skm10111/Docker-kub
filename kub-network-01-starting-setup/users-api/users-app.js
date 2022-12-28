a00b2678ed5c601d9617d1a7f85daf081010c82b
skm10111 <saurabh.realbotz@gmail.com>
2022-12-28 14:54:09 +0530

    pods networking

--- kub-network-01-starting-setup/users-api/users-app.js
+++ kub-network-01-starting-setup/users-api/users-app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   // It's just a dummy service - we don't really care for the email
   const email = req.body.email;
   const password = req.body.password;
@@ -19,23 +19,27 @@ app.post('/signup', async (req, res) => {
   ) {
     return res
       .status(422)
-      .json({ message: 'An email and password needs to be specified!' });
+      .json({ message: "An email and password needs to be specified!" });
   }
 
   try {
-    const hashedPW = await axios.get('http://auth/hashed-password/' + password);
+    const hashedPW = await axios.get(
+      `http://${process.env.AUTH_SERVICE_SERVICE_HOST}/hashed-password/` +
+        password
+    );
+    // const hashedPW = "dummy text";
     // since it's a dummy service, we don't really care for the hashed-pw either
     console.log(hashedPW, email);
-    res.status(201).json({ message: 'User created!' });
+    res.status(201).json({ message: "User created!" });
   } catch (err) {
     console.log(err);
     return res
       .status(500)
-      .json({ message: 'Creating the user failed - please try again later.' });
+      .json({ message: "Creating the user failed - please try again later." });
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   // It's just a dummy service - we don't really care for the email
   const email = req.body.email;
   const password = req.body.password;
@@ -48,18 +52,22 @@ app.post('/login', async (req, res) => {
   ) {
     return res
       .status(422)
-      .json({ message: 'An email and password needs to be specified!' });
+      .json({ message: "An email and password needs to be specified!" });
   }
 
   // normally, we'd find a user by email and grab his/ her ID and hashed password
-  const hashedPassword = password + '_hash';
+  const hashedPassword = password + "_hash";
+  // const response = { status: 200, data: { token: "abc" } };
   const response = await axios.get(
-    'http://auth/token/' + hashedPassword + '/' + password
+    `http://${process.env.AUTH_ADDRESS}/token/` +
+      hashedPassword +
+      "/" +
+      password
   );
   if (response.status === 200) {
     return res.status(200).json({ token: response.data.token });
   }
-  return res.status(response.status).json({ message: 'Logging in failed!' });
+  return res.status(response.status).json({ message: "Logging in failed!" });
 });
 
 app.listen(8080);
